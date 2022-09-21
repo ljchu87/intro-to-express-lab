2b181b53bbd9cc7144838d8d04a49a6aa71c9c99
Leon Chu <Ljchu87@gmail.com>
2022-09-20 19:23:13 -0500

    intro to express lab complete

---
+++ data/students.js
@@ -0,0 +1,9 @@
+const students = [
+  {text: 'Leon Chu', done: true, _id: 455566},
+  {text: 'John Doe', done: false, _id: 259988},
+  {text: 'Jane Doe', done: false, _id: 998866},
+]
+
+export { 
+	students
+}
---
+++ server.js
@@ -0,0 +1,37 @@
+// import modules
+
+import express from 'express'
+import { students } from './data/students.js'
+
+// Create Express app
+
+const app = express()
+
+// Configure the app (app.set)
+
+app.set('view engine', 'ejs')
+
+// Mount Middleware (app.use)
+
+
+// Mount routes
+
+app.get('/', function(req, res) {
+  res.redirect('/students')
+})
+
+app.get('/home', function(req, res) {
+  res.render('home')
+})
+
+app.get('/students', function(req, res) {
+  res.render('students/index', {
+    students: students
+  })
+})
+
+// Tell the app to listen on port 3000
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000')
+})
